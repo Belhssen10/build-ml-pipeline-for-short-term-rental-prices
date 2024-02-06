02af8d9f50f5c6a408d962dc60e999038cd44aff
b.ouerghi <b.ouerghi@instadeep.com>
2024-02-06 16:39:20 +0100

    'restrain_values'

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -32,6 +32,9 @@ def go(args):
     logger.info("Convert the column last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
     
+    # Restrain values
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     # Save the resulting dataframe into a csv file
     logger.info("Save the resulting dataframe into a csv file")
     df.to_csv("clean_sample.csv", index=False)
